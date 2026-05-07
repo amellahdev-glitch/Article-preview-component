d8bc6e870f824a6cc18f06f10385aec97a7b5bdc
Mohamed Amellah <amellah.dev@gmail.com>
2026-05-07 08:58:30 +0100

    finish with the desktop design

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const popUp = document.getElementById("sharePopup")
 const btn = document.getElementById("share")
 
-btn.addEventListener('click', () => {
-    popUp.classList.toggle("active")
-})
+btn.addEventListener("click", () => {
+
+    if (popUp.classList.contains("active")) {
+        popUp.classList.remove("active");
+    } else {
+        popUp.classList.add("active");
+    }
+
+});
